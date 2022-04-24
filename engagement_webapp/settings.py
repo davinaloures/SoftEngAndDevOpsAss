cbc197ea3c47dbc307b36bc012fee246666cfc32
Davina Loures <t-dalour@microsoft.com>
2022-04-24 23:56:43 +0100

    Updated settings

--- engagement_webapp/settings.py
+++ engagement_webapp/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a+c0yegh(9ia8kq2)@pbr0xuvf&h5o*!ht7@fus@su8&v_$%82'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['engagement-request-app.herokuapp.com']
 
 
 
