4919515422a1c6efbcdb6cb8dca8c9ccc8397e0a
Davina <t-dalour@microsoft.com>
2023-07-18 12:25:52 +0100

    change to apps.py

--- requests/apps.py
+++ requests/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RequestsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'requests'
+
+class BaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'base'
