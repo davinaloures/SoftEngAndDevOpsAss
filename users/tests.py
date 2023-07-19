adeb85550be53cfa3b5e997605c42f7105d6977a
Davina <t-dalour@microsoft.com>
2023-07-19 14:15:44 +0100

    test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+
 
 # Create your tests here.
+import django
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engagement_webapp.settings')
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'engagement_webapp.settings'
+django.setup()
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
